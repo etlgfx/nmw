bf096d70980dad714eebde5bb2b132d2f16eb402
Eric Liang <etlgfx@gmail.com>
2012-10-09 12:24:46 -0400

    fancy render loop

--- game.js
+++ game.js
@@ -1,24 +1,44 @@
-var game = (function () {
-	var canvas,
-		context;
+var canvas,
+	context;
 
-	function game(id) {
-		canvas = document.getElementById(id);
-		context = canvas.getContext('2d');
+function obj() {
+	this.coords = [0, 0];
+	this.size = [50, 50];
+	this.color = [0, 0, 0];
+}
 
-		requestAnimationFrame(step);
-	};
+obj.prototype.render = function(ctx) {
+	ctx.fillStyle = "rgb("+ this.color.join(",") +")";
+	ctx.fillRect(this.coords[0], this.coords[1], this.size[0], this.size[1]);
+};
 
-	function step() {
-		context.fillStyle = "rgb(200,0,0)";
-		context.fillRect (10, 10, 55, 50);
+function game(id) {
+	canvas = document.getElementById(id);
+	context = canvas.getContext('2d');
+	canvas.addEventListener('click', function(evt) {
+		//console.log(evt);
+	});
 
-		context.fillStyle = "rgba(0, 0, 200, 0.5)";
-		context.fillRect (30, 30, 55, 50);
-	};
+	this.objs = [];
 
-	return game;
-})();
+	var o;
+
+	o = new obj();
+	o.coords = [50, 50];
+	this.objs.push(o);
+
+	o = new obj();
+	o.coords = [150, 150];
+	o.color = [50, 200, 0];
+	this.objs.push(o);
+
+	var self = this;
+	requestAnimationFrame(function () { self.step() }, canvas);
+}
+
+game.prototype.step = function () {
+	this.objs.forEach(function (obj) { obj.render(context); });
+};
 
 (function() {
 	var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
