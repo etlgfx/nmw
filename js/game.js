dcbe2770c24d2cb8c9a4063b37c4782d5c0e94aa
Eric Liang <etlgfx@gmail.com>
2012-10-11 10:38:10 -0400

    changing whitespace to spaces

--- js/game.js
+++ js/game.js
@@ -1,260 +1,260 @@
 var Game = (function () {
 
-	"use strict";
+    "use strict";
 
-	var canvas, context;
-	
-	/**
-	* Obj is the fundamental object from which all
-	* other game objects are polymorphed.
-	*/
-	function Obj () {
-		this.coords = [0, 0];
-		this.size = [50, 50];
-		this.color = [0, 0, 0];
-	}
+    var canvas, context;
 
-	Obj.prototype.render = function (ctx, timing) {
-		if (this.animating && this.animating.time > 0) {
-			if (this.animating.velocity) {
-				this.coords[0] += this.animating.velocity[0] * timing.step;
-				this.coords[1] += this.animating.velocity[1] * timing.step;
-			}
-			else {
-				this.animating.velocity = [
-					(this.animating.to[0] - this.coords[0]) / this.animating.time,
-					(this.animating.to[1] - this.coords[1]) / this.animating.time,
-				];
-			}
+    /**
+     * Obj is the fundamental object from which all
+     * other game objects are polymorphed.
+     */
+    function Obj () {
+        this.coords = [0, 0];
+        this.size = [50, 50];
+        this.color = [0, 0, 0];
+    }
 
-			this.animating.time -= timing.step;
-		}
-		else if (this.animating) {
-			this.coords = this.animating.to;
-			this.animating = undefined;
-		}
+    Obj.prototype.render = function (ctx, timing) {
+        if (this.animating && this.animating.time > 0) {
+            if (this.animating.velocity) {
+                this.coords[0] += this.animating.velocity[0] * timing.step;
+                this.coords[1] += this.animating.velocity[1] * timing.step;
+            }
+            else {
+                this.animating.velocity = [
+                    (this.animating.to[0] - this.coords[0]) / this.animating.time,
+                    (this.animating.to[1] - this.coords[1]) / this.animating.time,
+                    ];
+            }
 
-		ctx.fillStyle = "rgb("+ this.color.join(",") +")";
-		ctx.fillRect(this.coords[0], this.coords[1], this.size[0], this.size[1]);
-	};
+            this.animating.time -= timing.step;
+        }
+        else if (this.animating) {
+            this.coords = this.animating.to;
+            this.animating = undefined;
+        }
 
-	Obj.prototype.click = function (game, scene) {
-	};
+        ctx.fillStyle = "rgb("+ this.color.join(",") +")";
+        ctx.fillRect(this.coords[0], this.coords[1], this.size[0], this.size[1]);
+    };
 
-	Obj.prototype.hover = function (game, scene) {
-	};
-	
-	Obj.prototype.animate = function (to, time) {
-		if (time === undefined)
-			time = 1000;
+    Obj.prototype.click = function (game, scene) {
+    };
 
-		this.animating = {
-			'to': to,
-			'time': time
-		};
-	};
+    Obj.prototype.hover = function (game, scene) {
+    };
 
-	/**
-	* Obj.Button makes for nice buttons to you for click.
-	*/
-	function Button (options) {
-		Obj.call(this);
+    Obj.prototype.animate = function (to, time) {
+        if (time === undefined)
+            time = 1000;
 
-		this.size = [100, 30];
-		this.title = options.title;
-		this.state = options.state;
-	}
+        this.animating = {
+            'to': to,
+            'time': time
+        };
+    };
 
-	Button.prototype = new Obj();
+    /**
+     * Obj.Button makes for nice buttons to you for click.
+     */
+    function Button (options) {
+        Obj.call(this);
 
-	Button.prototype.click = function (game, scene) {
-		game.pushState(game.loadState(this.state));
-	};
+        this.size = [100, 30];
+        this.title = options.title;
+        this.state = options.state;
+    }
 
-	Button.prototype.render = function (ctx, timing) {
-		Obj.prototype.render.call(this, ctx, timing);
+    Button.prototype = new Obj();
 
-		ctx.font = "400 16px sans-serif";
-		ctx.textAlign = "center";
+    Button.prototype.click = function (game, scene) {
+        game.pushState(game.loadState(this.state));
+    };
 
-		ctx.fillStyle = "rgb(255, 255, 255)";
-		ctx.fillText(this.title, this.coords[0] + this.size[0] / 2, this.coords[1] + 0.7 * this.size[1]);
-	};
+    Button.prototype.render = function (ctx, timing) {
+        Obj.prototype.render.call(this, ctx, timing);
 
-	/**
-	* Obj.Building is a standard in game object.
-	* it cannot move
-	* it can store other objects
-	* it has a health meter from 0 to 100 
-	* 
-	*/
-	function Building (options) {
-		Obj.call(this);
-		this.size = [100, 30];
-		this.title = 'building';
-		this.textColor = "rgb(255, 255, 255)";
-	}
+        ctx.font = "400 16px sans-serif";
+        ctx.textAlign = "center";
 
-	Building.prototype = new Obj();
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        ctx.fillText(this.title, this.coords[0] + this.size[0] / 2, this.coords[1] + 0.7 * this.size[1]);
+    };
 
-	Building.prototype.render = function (ctx, timing) {
-		Obj.prototype.render.call(this, ctx, timing);
+    /**
+     * Obj.Building is a standard in game object.
+     * it cannot move
+     * it can store other objects
+     * it has a health meter from 0 to 100 
+     * 
+     */
+    function Building (options) {
+        Obj.call(this);
+        this.size = [100, 30];
+        this.title = 'building';
+        this.textColor = "rgb(255, 255, 255)";
+    }
 
-		ctx.font = "400 16px sans-serif";
-		ctx.textAlign = "center";
+    Building.prototype = new Obj();
 
-		ctx.fillStyle = this.textColor;
-		ctx.fillText(this.title, this.coords[0] + this.size[0] / 2, this.coords[1] + 0.7 * this.size[1]);
-	};
+    Building.prototype.render = function (ctx, timing) {
+        Obj.prototype.render.call(this, ctx, timing);
 
-	Building.prototype.hover = function (game, scene) {
-		this.textColor = "rgb(255, 0, 0)";
-	};
+        ctx.font = "400 16px sans-serif";
+        ctx.textAlign = "center";
 
-	/**
-	* Obj.Scene holds all of the game objects
-	*/
-	function Scene () {
-		this.objs = [];
-	}
+        ctx.fillStyle = this.textColor;
+        ctx.fillText(this.title, this.coords[0] + this.size[0] / 2, this.coords[1] + 0.7 * this.size[1]);
+    };
 
-	Scene.prototype.add = function (obj) {
-		this.objs.push(obj);
+    Building.prototype.hover = function (game, scene) {
+        this.textColor = "rgb(255, 0, 0)";
+    };
 
-		return obj;
-	};
+    /**
+     * Obj.Scene holds all of the game objects
+     */
+    function Scene () {
+        this.objs = [];
+    }
 
-	Scene.prototype.queryHits = function (coords) {
-		var hits = this.objs.filter(function (obj) {
-			return coords[0] >= obj.coords[0] && coords[0] <= (obj.coords[0] + obj.size[0]) &&
-				coords[1] >= obj.coords[1] && coords[1] <= (obj.coords[1] + obj.size[1])
-		});
+    Scene.prototype.add = function (obj) {
+        this.objs.push(obj);
 
-		return hits;
-	};
+        return obj;
+    };
 
-	Scene.prototype.render = function (ctx, timing) {
-		ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    Scene.prototype.queryHits = function (coords) {
+        var hits = this.objs.filter(function (obj) {
+            return coords[0] >= obj.coords[0] && coords[0] <= (obj.coords[0] + obj.size[0]) &&
+            coords[1] >= obj.coords[1] && coords[1] <= (obj.coords[1] + obj.size[1])
+        });
 
-		this.objs.forEach(function (obj) { obj.render(ctx, timing); });
-	};
+        return hits;
+    };
 
-	Scene.prototype.flush = function () {
-		this.objs = [];
-	};
-	
-	/**
-	* Game is the objecct that runs the loop and 
-	* makes the game universe go.
-	*/
-	function Game (id) {
-		canvas = document.getElementById(id);
-		context = canvas.getContext('2d');
+    Scene.prototype.render = function (ctx, timing) {
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-		this.sceneStack = [];
-		this.scene = null;
-		this.timing = {
-			'start': Date.now(),
-			'last': Date.now(),
-		};
-		this.mouseHandle = {
-			'coords': [0,0],
-			'lastCoords': [0,0],
-		};		
+        this.objs.forEach(function (obj) { obj.render(ctx, timing); });
+    };
+
+    Scene.prototype.flush = function () {
+        this.objs = [];
+    };
+
+    /**
+     * Game is the objecct that runs the loop and 
+     * makes the game universe go.
+     */
+    function Game (id) {
+        canvas = document.getElementById(id);
+        context = canvas.getContext('2d');
+
+        this.sceneStack = [];
+        this.scene = null;
+        this.timing = {
+            'start': Date.now(),
+            'last': Date.now(),
+        };
+        this.mouseHandle = {
+            'coords': [0,0],
+            'lastCoords': [0,0],
+        };		
 
 
-		canvas.addEventListener('click', this.clickController.bind(this));
-		canvas.addEventListener('mousemove', this.mouseMoveController.bind(this));
+        canvas.addEventListener('click', this.clickController.bind(this));
+        canvas.addEventListener('mousemove', this.mouseMoveController.bind(this));
 
-		requestAnimationFrame(this.step.bind(this), canvas);
-	}
+        requestAnimationFrame(this.step.bind(this), canvas);
+    }
 
-	Game.prototype.step = function () {
-		this.timing.step = Date.now() - this.timing.last;
-		this.timing.last = Date.now();
+    Game.prototype.step = function () {
+        this.timing.step = Date.now() - this.timing.last;
+        this.timing.last = Date.now();
 
-		if((this.mouseHandle.lastCoords[0] != this.mouseHandle.coords[0]) || (this.mouseHandle.lastCoords[1] != this.mouseHandle.coords[1])) 
-		{
-			var hits = this.scene.queryHits(this.mouseHandle.coords);
-			
-			hits.forEach(function (hit) { hit.hover(this, this.scene); }, this);
-		}	
+        if((this.mouseHandle.lastCoords[0] != this.mouseHandle.coords[0]) || (this.mouseHandle.lastCoords[1] != this.mouseHandle.coords[1])) 
+        {
+            var hits = this.scene.queryHits(this.mouseHandle.coords);
 
-		if (this.scene)
-			this.scene.render(context, this.timing);
+            hits.forEach(function (hit) { hit.hover(this, this.scene); }, this);
+        }	
 
-		requestAnimationFrame(this.step.bind(this), canvas);
-	};
+        if (this.scene)
+            this.scene.render(context, this.timing);
 
-	Game.prototype.clickController = function (evt) {
-		if (!this.scene)
-			return;
+        requestAnimationFrame(this.step.bind(this), canvas);
+    };
 
-		var coords = [evt.pageX - evt.target.offsetLeft, evt.pageY - evt.target.offsetTop];
-		var hits = this.scene.queryHits(coords);
+    Game.prototype.clickController = function (evt) {
+        if (!this.scene)
+            return;
 
-		//console.log(hits);
-		hits.forEach(function (hit) { hit.click(this, this.scene); }, this);
-	};
-	
-	Game.prototype.mouseMoveController = function (evt) {
-		if(!this.scene)
-			return;		
-		
-		this.mouseHandle.lastCoords = this.mouseHandle.coords;
-		this.mouseHandle.coords = [evt.pageX - evt.target.offsetLeft, evt.pageY - evt.target.offsetTop];
-	}
+        var coords = [evt.pageX - evt.target.offsetLeft, evt.pageY - evt.target.offsetTop];
+        var hits = this.scene.queryHits(coords);
 
-	Game.prototype.loadState = function (stateFile) {
-		var scene = new Scene();
+        //console.log(hits);
+        hits.forEach(function (hit) { hit.click(this, this.scene); }, this);
+    };
 
-		ejs.xhr('GET').callback((function (xhr, data) {
-			if (data.menu) {
-				data.menu.options.forEach(function (option) {
-					scene.add(new Button(option)).animate([200,200], 500);
-				}, this);
-			}
+    Game.prototype.mouseMoveController = function (evt) {
+        if(!this.scene)
+            return;
 
-			if (data.units) {
-				data.units.forEach(function (unit) {
-					scene.add(new Obj()).coords = unit.coords;
-				}, this);
-			}
+        this.mouseHandle.lastCoords = this.mouseHandle.coords;
+        this.mouseHandle.coords = [evt.pageX - evt.target.offsetLeft, evt.pageY - evt.target.offsetTop];
+    }
 
-			
-			if (data.buildings) {
-				data.buildings.forEach(function (building) {
-					scene.add(new Building()).coords = building.coords;
-				}, this);
-			}
-		}).bind(this)).send(stateFile);
+    Game.prototype.loadState = function (stateFile) {
+        var scene = new Scene();
 
-		return scene;
-	};
+        ejs.xhr('GET').callback((function (xhr, data) {
+            if (data.menu) {
+                data.menu.options.forEach(function (option) {
+                    scene.add(new Button(option)).animate([200,200], 500);
+                }, this);
+            }
 
-	Game.prototype.popState = function () {
-		var lastScene = this.sceneStack.pop();
+            if (data.units) {
+                data.units.forEach(function (unit) {
+                    scene.add(new Obj()).coords = unit.coords;
+                }, this);
+            }
 
-		this.scene = this.sceneStack[this.sceneStack.length - 1];
 
-		return lastScene;
-	};
+            if (data.buildings) {
+                data.buildings.forEach(function (building) {
+                    scene.add(new Building()).coords = building.coords;
+                }, this);
+            }
+        }).bind(this)).send(stateFile);
 
-	Game.prototype.pushState = function (scene) {
-		if (!scene instanceof Scene) {
-			throw "not a scene";
-		}
+        return scene;
+    };
 
-		this.sceneStack.push(scene);
-		this.scene = scene;
+    Game.prototype.popState = function () {
+        var lastScene = this.sceneStack.pop();
 
-		return scene;
-	};
+        this.scene = this.sceneStack[this.sceneStack.length - 1];
 
-	return Game;
+        return lastScene;
+    };
+
+    Game.prototype.pushState = function (scene) {
+        if (!scene instanceof Scene) {
+            throw "not a scene";
+        }
+
+        this.sceneStack.push(scene);
+        this.scene = scene;
+
+        return scene;
+    };
+
+    return Game;
 })();
 
 (function () {
-	var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
-	window.requestAnimationFrame = requestAnimationFrame;
+    var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+    window.requestAnimationFrame = requestAnimationFrame;
 })();
